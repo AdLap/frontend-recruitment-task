3cf6685d20c7371ae4c1e203ae323e7fc7e919f4
Adam Łapacz <alapacz@netis.pl>
2022-06-04 23:40:03 +0200

    Initial structure of module

---
+++ src/js/main.js
@@ -0,0 +1,101 @@
+const app = document.querySelector('#app')
+
+class Popup {
+    constructor(parentElement, popupContent) {
+        this.parentElement = parentElement
+        this.popupContent = popupContent
+        this.popupWrapper = null
+        this.popupCloseButton = null
+
+        this.createPopup()
+    }
+
+    createPopup() {
+        this.popupWrapper = document.createElement('div')
+        this.popupWrapper.classList.add('popup')
+        this.parentElement.prepend(this.popupWrapper)
+        this.popupWrapper.onclick = (event) => this.onClosePopup(event)
+
+        const popup = document.createElement('div')
+        popup.classList.add('popup__window')
+        this.popupWrapper.append(popup)
+
+        const popupTitle = document.createElement('h1')
+        popupTitle.classList.add('popup__window__title')
+        popupTitle.innerText = 'Alert!'
+        popup.append(popupTitle)
+
+        const popupContent = document.createElement('p')
+        popupContent.classList.add('popup__window__content')
+        popupContent.innerText = this.popupContent
+        popup.append(popupContent)
+
+        this.popupCloseButton = document.createElement('div')
+        this.popupCloseButton.classList.add('popup__window__close')
+        popup.append(this.popupCloseButton)
+    }
+
+    onClosePopup(event) {
+        if (event.target === this.popupWrapper || event.target === this.popupCloseButton) {
+            this.popupWrapper.remove()
+        }
+    }
+}
+
+class Section {
+    constructor(parentElement) {
+        this.counter = 0
+        this.parentElement = parentElement
+
+        this.createSection()
+    }
+
+    createSection() {
+        const section = document.createElement('section')
+        section.classList.add('container')
+        this.parentElement.append(section)
+
+        const imageWrapper = document.createElement('div')
+        imageWrapper.classList.add('container__image')
+        section.append(imageWrapper)
+
+        const picture = document.createElement('img')
+        picture.classList.add('container__image__picture')
+        imageWrapper.append(picture)
+
+        const content = document.createElement('article')
+        content.classList.add('container__content')
+        section.append(content)
+
+        const sectionTitle = document.createElement('h1')
+        sectionTitle.classList.add('container__content__title')
+        sectionTitle.innerText = 'Lorem Ipsum'
+        content.append(sectionTitle)
+
+        const paragraph = document.createElement('p')
+        paragraph.classList.add('container__content__paragraph')
+        paragraph.innerText = 'Infinitely scalable, feature-rich and cloud-native data management and protection for modern and legacy infrastructures and SaaS platforms, managed via a single app with no hardware required.'
+        content.append(paragraph)
+
+        const button = document.createElement('button')
+        button.classList.add('container__content__button')
+        button.type = 'button'
+        button.innerText = 'Button'
+        button.setAttribute('aria-label', 'Click me')
+        content.append(button)
+        button.onclick = () => this.onOpenPopup()
+
+    }
+
+    onOpenPopup() {
+        ++this.counter
+        if (this.counter >= 5) {
+            this.counter = 0
+        }
+
+        return new Popup(this.parentElement, `You have clicked ${this.counter} times to related button.`)
+    }
+}
+
+const firstSection = new Section(app)
+const secondSection = new Section(app)
